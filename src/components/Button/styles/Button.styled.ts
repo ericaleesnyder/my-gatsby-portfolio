f0cb052ec00135b18234552b30d32c073f3b4aa0
ericasnyder <esnyder@webstacks.com>
2024-05-26 16:58:55 -0700

    build: button

--- src/components/Button/styles/Button.styled.ts
+++ src/components/Button/styles/Button.styled.ts
@@ -1,75 +1,127 @@
-import styled, { css } from "styled-components";
-import { color } from "../../../atoms/colors";
-import { font } from "../../../atoms/typography";
+import { Link } from 'gatsby';
+import styled from 'styled-components';
 
-// TODO: fix button styles - inaccessible contrast right now
+import { type ColorValues, color } from 'atoms/colors';
+import { font } from 'atoms/typography';
 
-type Props = {
-  variant: "primary" | "secondary" | "tertiary" | "inline";
-};
+import type { ButtonProps } from 'components/Button/Button';
 
-export const ButtonWrap = styled.div<Props>`
-  a {
-    display: inline-block;
+
+interface StyledButtonProps extends Omit<ButtonProps, 'to' | 'children' | 'hoverColor'> {
+  hoverColor?: ColorValues;
+}
+
+export const PillWrap = styled.div<StyledButtonProps>`
+  position: relative;
+  min-width: fit-content;
+  cursor: pointer;
+  :hover > div {
+    left: -4px;
+    bottom: 7px;
+    ::before {
+      content: '';
+      left: 2px;
+      bottom: -9px;
+      width: 100%;
+      background-color: ${({ hoverColor }) => hoverColor ? hoverColor : color.blue};
+      border-radius: 40px;
+      border: 2px solid ${color.black};
+      z-index: -2;
+    }
+  }
+  @media (min-width: 768px) {
+    width: fit-content;
+  }
+`
+
+export const Pill = styled.div<StyledButtonProps>`
+  ${({ size }) => size && size === 'Small' ? font('button', 'sm', 600) : font('button', 'lg', 600)};
+  color: ${({ isDark }) => isDark ? color.white : color.black };
+  text-align: center;
+  padding: ${({ size }) => size && size === 'Small' ? '12px 24px' : '16px 40px'};
+  border-radius: 40px;
+  border: 2px solid ${color.black};
+  background-color: ${({ isDark }) => isDark ? color.black : color.white };
+  position: relative;
+  left: 0px;
+  bottom: 0px;
+  white-space: nowrap;
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: fit-content;
+  :focus-visible,
+  :focus {
+    outline: -webkit-focus-ring-color auto 1px;
+    outline-color: ${color.black};
+    outline-offset: 5px;
+  }
+  ::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
     width: 100%;
     height: 100%;
   }
-  ${font("text", "med", 600)}
-  padding: 8px 12px;
-  border-radius: 20px;
+`;
+
+
+export const IconBtnWrap = styled.div<StyledButtonProps>`
+  position: relative;
   width: fit-content;
-  text-decoration: none;
   cursor: pointer;
-  transition: color 150ms ease, background-color 150ms ease, border 150ms ease;
-  &:focus-visible,
+  :hover > div {
+    left: -4px;
+    bottom: 7px;
+    ::before {
+      content: '';
+      left: 2px;
+      bottom: -9px;
+      width: 100%;
+      height: 100%;
+      background-color: ${color.blue};
+      border-radius: 8px;
+      border: 2px solid ${color.black};
+      z-index: -2;
+    }
+  }
+`
+
+export const IconBtn = styled.div<StyledButtonProps>`
+  svg {
+    height: 32px;
+    width: 32px;
+  }
+  display: block;
+  position: relative;
+  border: 2px solid ${color.black};
+  padding: 12px;
+  border-radius: 8px;
+  background-color: ${color.white};
+  width: fit-content;
+  :focus-visible,
   :focus {
     outline: -webkit-focus-ring-color auto 1px;
-    outline-color: ${color.common.flamingo};
+    outline-color: ${color.black};
+    outline-offset: 5px;
   }
-  ${(props) =>
-    (props.variant &&
-      props.variant === "primary" &&
-      css`
-        color: ${color.common.midnight};
-        border: 2px solid ${color.common.sunshine};
-        background-color: ${color.common.sunshine};
-        &:hover {
-          background-color: ${color.common.marigold};
-          border: 2px solid ${color.common.marigold};
-          color: ${color.common.light};
-        }
-      `) ||
-    (props.variant === "secondary" &&
-      css`
-        color: ${color.common.midnight};
-        border: 2px solid ${color.common.midnight};
-        &:hover {
-          color: ${color.common.lavender}; // FIXME
-          border: 2px solid ${color.common.lavender}; // FIXME
-        }
-      `) ||
-    (props.variant === "tertiary" &&
-      css`
-        color: ${color.common.midnight};
-        background-color: ${color.common.baby};
-        border: 2px solid ${color.common.baby};
-        &:hover {
-          border: 2px solid ${color.common.marigold};
-          background-color: ${color.common.marigold};
-          color: ${color.common.light};
-        }
-      `) ||
-    (props.variant === "inline" &&
-      css`
-        padding: 0;
-        color: ${color.common.lavender};
-        text-decoration: underline;
-        &:hover {
-          color: ${color.common.midnight};
-        }
-        &:focus-visible,
-        :focus {
-          border-radius: 0;
-        }
-      `)}
-`;
+  ::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 100%;
+  }
+`
+
+export const TextLink = styled(Link)`
+  transition: text-decoration 550ms ease;
+  ${font('text', 'lg', 600)}
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`
