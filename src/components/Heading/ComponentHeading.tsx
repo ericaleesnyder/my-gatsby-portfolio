205c491b60b84e3e17d072f619cbeaefa89927f8
ericasnyder <esnyder@webstacks.com>
2024-05-26 19:45:47 -0700

    build: heading

---
+++ src/components/Heading/ComponentHeading.tsx
@@ -0,0 +1,69 @@
+import React, { type FC } from 'react';
+import {
+  StructuredText,
+  type StructuredTextGraphQlResponse,
+} from 'react-datocms/structured-text';
+
+import Heading, { type HeadingTypes } from 'atoms/Text/Heading';
+import Text from 'atoms/Text/Text';
+
+import Button from 'components/Button/Button';
+import {
+  CtaWrap,
+  HeadingWrap,
+} from 'components/Heading/styles/ComponentHeading.styled';
+
+import type { DatoCmsComponentCallToAction } from 'graphqlTypes';
+
+interface HeadingProps {
+  heading?: string | null;
+  headingTag?: HeadingTypes | null;
+  headingSize?: 'xs' | 'sm' | 'med' | 'lg' | 'xl' | 'xxl' | null;
+  subheading?: string | null;
+  subheadingSize?: 'sm' | 'med' | 'lg' | 'xl' | 'xxl' | null;
+  subheadingWeight?: 300 | 400 | 500 | 600 | 700 | 800 | 900 | null;
+  ctas?: DatoCmsComponentCallToAction[] | null | undefined;
+  body?: StructuredTextGraphQlResponse | null;
+}
+
+const ComponentHeading: FC<HeadingProps> = ({
+  heading,
+  headingSize,
+  headingTag,
+  subheading,
+  subheadingSize,
+  subheadingWeight,
+  ctas,
+  body,
+}) => (
+  <HeadingWrap>
+    {heading && (
+      <Heading hTag={headingTag ?? 'h1'} size={headingSize}>
+        {heading}
+      </Heading>
+    )}
+    {subheading && (
+      <Text size={subheadingSize ?? 'xxl'} weight={subheadingWeight ?? 600}>
+        {subheading}
+      </Text>
+    )}
+    {body && <StructuredText data={body} />}
+    {ctas && ctas.length > 0 && (
+      <CtaWrap>
+        {ctas.map((cta) => (
+          <Button
+            to={cta?.link ?? ''}
+            hoverColor={cta?.hoverColor}
+            key={cta?.id ?? Math.random()}
+            size={cta?.size}
+            isDark={cta?.isDark}
+          >
+            {cta?.label}
+          </Button>
+        ))}
+      </CtaWrap>
+    )}
+  </HeadingWrap>
+);
+
+export default ComponentHeading;
