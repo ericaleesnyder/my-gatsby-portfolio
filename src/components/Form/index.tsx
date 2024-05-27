638f2689e2c886c80d49d826fbb0bd6a2c601f00
ericasnyder <esnyder@webstacks.com>
2024-05-26 19:46:39 -0700

    build: form

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,103 @@
+import Container from 'basics/Container.styled';
+import Section from 'basics/Section.styled';
+import React, { type FC, useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+import Text from 'atoms/Text/Text';
+import { color } from 'atoms/colors';
+
+import Button from 'components/Button/Button';
+import {
+  Form,
+  Input,
+  NameGroup,
+  TextArea,
+  ThankYou,
+  Wrapper,
+} from 'components/Form/styles/Form.styled';
+import ComponentHeading from 'components/Heading/ComponentHeading';
+
+import type { DatoCmsComponentCallToAction } from 'graphqlTypes';
+
+interface FormProps {}
+
+// TODO: complete form interaction. SendGrid.
+
+const ContactForm: FC<FormProps> = () => {
+  const [submitted, setSubmitted] = useState<boolean>(false);
+  // const [formData, setFormData] = useState();
+
+  const {
+    // control,
+    // handleSubmit,
+    register,
+    // formState: { isSubmitting, errors },
+    // reset,
+  } = useForm({
+    mode: 'all',
+  });
+
+  useEffect(() => {
+    console.log('submitted', submitted);
+  }, [submitted]);
+
+  return (
+    <Section id='contact-me-form'>
+      <Container>
+        <Wrapper>
+          <ComponentHeading
+            heading='Contact Me'
+            headingTag='h2'
+            headingSize='lg'
+            subheading="Shoot me a message any time with comments, questions, offers, or just to say hi. While I'm primarily interested in full-time employment, I'm open to opportunities of any kind."
+            subheadingSize='lg'
+            subheadingWeight={400}
+            ctas={[
+              {
+                link: '#',
+                label: 'My Resume',
+                size: 'Small',
+                isDark: false,
+              } as DatoCmsComponentCallToAction,
+            ]}
+          />
+          <Form>
+            {!submitted ? (
+              <>
+                <NameGroup>
+                  <Input {...register('firstName')} placeholder='First Name' />
+                  <Input {...register('lastName')} placeholder='Last Name' />
+                </NameGroup>
+                <Input {...register('email')} placeholder='Email' />
+                <Input {...register('subject')} placeholder='Subject' />
+                <TextArea {...register('message')} placeholder='Message' />
+                <Button
+                  hoverColor={color.blue}
+                  size='Large'
+                  isDark
+                  onSubmit={() => setSubmitted(true)}
+                >
+                  Send Message
+                </Button>
+              </>
+            ) : (
+              <ThankYou>
+                <Text size='xxl'>Thank you for your submission!</Text>
+                <Button
+                  hoverColor={color.blue}
+                  size='Small'
+                  isDark
+                  onSubmit={() => setSubmitted(false)}
+                >
+                  Back
+                </Button>
+              </ThankYou>
+            )}
+          </Form>
+        </Wrapper>
+      </Container>
+    </Section>
+  );
+};
+
+export default ContactForm;
---
+++ src/components/Form/styles/Form.styled.ts
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+import { color } from 'atoms/colors';
+import font from 'atoms/typography';
+
+export const Wrapper = styled.div`
+  display: grid;
+  gap: 24px;
+  grid-template-columns: 1fr;
+  @media (min-width: 993px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
+export const Form = styled.form`
+  display: grid;
+  gap: 16px;
+  .pill,
+  div {
+    width: 100%;
+  }
+  input,
+  textarea {
+    ${font('text', 'med', 400)}
+  }
+`;
+
+export const NameGroup = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: 1fr;
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
+export const Input = styled.input`
+  border-radius: 8px;
+  border: 2px solid ${color.black};
+  padding: 16px;
+`;
+
+export const TextArea = styled.textarea`
+  border-radius: 8px;
+  border: 2px solid ${color.black};
+  padding: 16px;
+  min-height: 150px;
+`;
+
+export const ThankYou = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
+  margin: auto 0;
+  .pill,
+  div {
+    width: unset;
+  }
+`
